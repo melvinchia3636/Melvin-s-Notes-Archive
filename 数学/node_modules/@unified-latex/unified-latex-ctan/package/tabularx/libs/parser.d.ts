71b2baaf23d37d48e8325d822efc420e5149072d
melvinchia3636 <melvinchia623600@gmail.com>
2022-10-09 12:22:29 +0800

    aww

--- 数学/node_modules/@unified-latex/unified-latex-ctan/package/tabularx/libs/parser.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Ast from "@unified-latex/unified-latex-types";
-import * as TabularSpec from "./types";
-/**
- * Parse a tabular/tabularx specification, e.g. `"|c|r|r|"`. This parser assumes the specification has
- * already been parsed as LaTeX.
- */
-export declare function parseTabularSpec(ast: Ast.Node[]): TabularSpec.TabularColumn[];
-//# sourceMappingURL=parser.d.ts.map
--- 数学/node_modules/@unified-latex/unified-latex-ctan/package/tabularx/libs/print-raw.d.ts
+++
@@ -1,6 +0,0 @@
-import * as TabularSpec from "./types";
-/**
- * Print a tabular/tabularx argument specification AST to a string.
- */
-export declare function printRaw(node: TabularSpec.Ast, root?: boolean): string;
-//# sourceMappingURL=print-raw.d.ts.map
