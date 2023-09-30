84e76bdc28c7744798a1fff077c8fc007cb707d6
melvinchia3636 <melvinchia623600@gmail.com>
2023-09-30 21:18:02 +0800

    30 Sep 2023

--- generateTOC.py
+++ generateTOC.py
@@ -1,18 +1,23 @@
 import os
 
+EXCLUDED = ["readme.md", "generateTOC.py", ".DS_Store",
+            ".obsidian", ".git", ".gitignore", "LICENSE", "README.md"]
+EXCLUDED_EXTENSIONS = ["jpg", "png", "jpeg", "gif", "svg"]
+
 
 def listdir(stream, folder, indent=0):
-    for item in os.listdir(folder):
-        if os.path.isdir(os.path.join(folder, item)):
-            stream.write(
-                '<{}>\n<details>\n<summary>{}</summary>\n\n'.format(["dl", "dd"][indent % 2], item))
-            listdir(stream, os.path.join(folder, item), indent + 1)
-            stream.write(
-                "\n</details>\n</{}>\n\n".format(["dl", "dd"][indent % 2]))
-        else:
-            if folder != "readme.md":
-                stream.write("- [{}](<{}>)\n".format(item,
-                             os.path.join(folder, item)))
+    for item in sorted(os.listdir(folder)):
+        if item not in EXCLUDED:
+            if os.path.isdir(os.path.join(folder, item)):
+                stream.write(
+                    '<{}>\n<details>\n<summary>{}</summary>\n\n'.format(["dl", "dd"][indent % 2], item))
+                listdir(stream, os.path.join(folder, item), indent + 1)
+                stream.write(
+                    "\n</details>\n</{}>\n\n".format(["dl", "dd"][indent % 2]))
+            else:
+                if item.split(".")[-1] not in EXCLUDED_EXTENSIONS:
+                    stream.write("- [{}](<{}>)\n".format(item,
+                                                         os.path.join(folder, item)))
 
 
 with open("readme.md", "w") as file:
