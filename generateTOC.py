4ecb10917ba867a9e4d77da7ee0032a1097006ec
melvinchia3636 <melvinchia623600@gmail.com>
2023-09-30 21:06:07 +0800

    30 Sep 2023

--- generateTOC.py
+++ generateTOC.py
@@ -1,21 +1,18 @@
 import os
 
 
-def listdir(folder, level=0, files=[]):
-    for file in os.listdir(folder):
-        if os.path.isdir(os.path.join(folder, file)):
-            files.append([os.path.join(folder, file), level])
-            listdir(os.path.join(folder, file), level + 1, files)
+def listdir(stream, folder):
+    for item in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, item)):
+            stream.write('<details>\n<summary>{}</summary>\n\n'.format(item))
+            listdir(stream, os.path.join(folder, item))
+            stream.write("\n</details>\n\n")
         else:
-            files.append([os.path.join(folder, file), level])
-    return files
+            if folder != "readme.md":
+                stream.write("- [{}](<{}>)\n".format(item,
+                             os.path.join(folder, item)))
 
 
 with open("readme.md", "w") as file:
     file.write("# Table of Contents\n")
-    for folder in os.listdir("."):
-        if os.path.isdir(folder):
-            file.write('<details>\n<summary>{}</summary>\n\n'.format(folder))
-            file.write('\n'.join(["{}- [{}](<{}>)".format("  " * file[1], file[0].split("/")[-1], file[0])
-                       for file in listdir(folder, level=0, files=[])]))
-            file.write("\n</details>\n")
+    listdir(file, ".")
