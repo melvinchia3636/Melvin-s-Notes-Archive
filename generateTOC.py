403c72be57917b6e8a8a6af3326b2b122adcbe37
melvinchia3636 <melvinchia623600@gmail.com>
2023-09-30 21:08:47 +0800

    30 Sep 2023

--- generateTOC.py
+++ generateTOC.py
@@ -1,15 +1,16 @@
 import os
 
 
-def listdir(stream, folder):
+def listdir(stream, folder, indent=""):
     for item in os.listdir(folder):
         if os.path.isdir(os.path.join(folder, item)):
-            stream.write('<details>\n<summary>{}</summary>\n\n'.format(item))
-            listdir(stream, os.path.join(folder, item))
-            stream.write("\n</details>\n\n")
+            stream.write('{}<details>\n{}<summary>{}</summary>\n\n'.format(
+                         indent, indent, item))
+            listdir(stream, os.path.join(folder, item), indent + "  ")
+            stream.write("\n{}</details>\n\n".format(indent))
         else:
             if folder != "readme.md":
-                stream.write("- [{}](<{}>)\n".format(item,
+                stream.write("{}- [{}](<{}>)\n".format(indent, item,
                              os.path.join(folder, item)))
 
 
