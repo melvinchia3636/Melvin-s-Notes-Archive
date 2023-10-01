277a5f446052e7a7973314b3a8fda46579bbc35e
melvinchia3636 <melvinchia623600@gmail.com>
2023-10-01 08:51:00 +0800

    1 Oct 2023

--- generateTOC.py
+++ generateTOC.py
@@ -8,18 +8,30 @@ EXCLUDED_EXTENSIONS = ["jpg", "png", "jpeg",
 
 
 def listdir(stream, folder, indent=0):
-    for item in sorted(os.listdir(folder), key=lambda s: (os.path.isdir(os.path.join(folder, s)), int(re.findall(r"\d+", s)[0]) if re.findall(r"\d+", s) else 0, s.lower())):
+    for item in sorted(
+        os.listdir(folder), key=lambda s: (
+            os.path.isdir(os.path.join(folder, s)),
+            int(re.findall(r"\d+", s)[0]) if re.findall(r"\d+", s) else 0,
+            s.lower()
+        )
+    ):
         if item not in EXCLUDED:
             if os.path.isdir(os.path.join(folder, item)):
                 stream.write(
                     '{}\n<details>\n<summary>{}</summary>\n\n'.format("<dl><dd>"*indent, item))
                 listdir(stream, os.path.join(folder, item), indent + 1)
                 stream.write(
-                    "\n</details>\n{}\n{}\n".format("</dd></dl>"*indent, "<hr></hr>" if indent == 0 else ""))
+                    "\n</details>\n{}\n{}\n".format(
+                        "</dd></dl>"*indent,
+                        "<hr></hr>" if indent == 0 else ""
+                    )
+                )
             else:
                 if item.split(".")[-1] not in EXCLUDED_EXTENSIONS and not item.startswith("."):
                     stream.write("- [{}](<{}>)\n".format(item,
-                                                         os.path.join(folder, item)))
+                                                         os.path.join(
+                                                             folder, item)
+                                                         ))
 
 
 with open("readme.md", "w") as file:
